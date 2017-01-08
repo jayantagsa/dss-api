a614462b63f32635c24404b6684a4befbd5dfd3c
jayantasinha <jsinha@comtechllc.com>
2017-01-08 15:31:06 -0500

    Re-approval required

--- DSS-API/src/gov/gsa/dss/helper/staic/OrgCodes.groovy
+++ DSS-API/src/gov/gsa/dss/helper/staic/OrgCodes.groovy
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List
 
-import org.apache.log4j.Logger;
+import org.apache.log4j.Logger;  
 import org.json.JSONException;
 import org.json.JSONObject;
 
